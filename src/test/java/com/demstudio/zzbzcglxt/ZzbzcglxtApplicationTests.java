9564cb28ad79d217158afe4b5fe4e83074aa1b7f
office <461216936@qq.com>
2020-03-06 19:02:56 +0800

    2020-03-06  新建

---
+++ src/test/java/com/demstudio/zzbzcglxt/ZzbzcglxtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demstudio.zzbzcglxt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZzbzcglxtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
