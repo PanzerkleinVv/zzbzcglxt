eaa6a09acd471072da6aa6d14f62d7335c1ad635
office <461216936@qq.com>
2020-04-10 21:26:32 +0800

    2020-04-10 office

--- src/main/java/com/demstudio/zzbzcglxt/controller/UserController.java
+++ src/main/java/com/demstudio/zzbzcglxt/controller/UserController.java
@@ -2,90 +2,67 @@ package com.demstudio.zzbzcglxt.controller;
 
 import com.demstudio.zzbzcglxt.domain.User;
 import com.demstudio.zzbzcglxt.service.UserService;
+import com.demstudio.zzbzcglxt.vo.Message;
 import com.demstudio.zzbzcglxt.vo.PageRequest;
 import com.demstudio.zzbzcglxt.vo.PageResult;
-import com.demstudio.zzbzcglxt.vo.Message;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.shiro.SecurityUtils;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Resource
-    private UserService userService;
+  @Resource
+  private UserService userService;
 
-    @GetMapping("/index")
-    public String index() {
-        return "user";
-    }
+  @GetMapping("/list")
+  public PageResult list(PageRequest pageQuery) {
+    return userService.findPage(pageQuery);
+  }
 
-    @GetMapping("/list")
-    @ResponseBody
-    public PageResult list(PageRequest pageQuery) {
-        return userService.findPage(pageQuery);
+  @GetMapping("/reset")
+  public Message reset(String userId) {
+    User user = new User();
+    user.setUserId(userId);
+    user.setUserPsw(DigestUtils.sha256Hex("1234567890"));
+    if (userService.changePsw(user)) {
+      return new Message(true, "重置密码成功");
+    } else {
+      return new Message(false, "重置密码失败");
     }
+  }
 
-    @GetMapping("/reset")
-    @ResponseBody
-    public Message reset(String userId) {
-        User user = new User();
-        user.setUserId(userId);
-        user.setUserPsw(DigestUtils.sha256Hex("1234567890"));
-        if (userService.changePsw(user)) {
-            return new Message(true, "重置密码成功");
-        } else {
-            return new Message(false, "重置密码失败");
-        }
+  @PostMapping("/create")
+  public Message create(User user) {
+    user.setUserId(DigestUtils.sha1Hex(UUID.randomUUID().toString()));
+    user.setUserPsw(DigestUtils.sha256Hex("1234567890"));
+    if (userService.createUser(user)) {
+      return new Message(true, "新增用户成功");
+    } else {
+      return new Message(false, "新增用户失败");
     }
+  }
 
-    @PostMapping("/create")
-    @ResponseBody
-    public Message create(User user) {
-        user.setUserId(DigestUtils.sha1Hex(UUID.randomUUID().toString()));
-        user.setUserPsw(DigestUtils.sha256Hex("1234567890"));
-        if (userService.createUser(user)) {
-            return new Message(true, "新增用户成功");
-        } else {
-            return new Message(false, "新增用户失败");
-        }
-    }
+  @GetMapping("/checkUserName")
+  public Boolean checkUserName(String userName) {
+    return userService.getUserByUserName(userName) == null;
+  }
 
-    @GetMapping("/checkUserName")
-    @ResponseBody
-    public Boolean checkUserName(String userName) {
-        return userService.getUserByUserName(userName) == null;
+  @PostMapping("/changePsw")
+  public Message changePsw(String userPsw) {
+    final User me = (User) SecurityUtils.getSubject().getPrincipal();
+    System.out.println("-------------" + userPsw);
+    User user = new User();
+    user.setUserId(me.getUserId());
+    user.setUserPsw(userPsw);
+    if (userService.changePsw(user)) {
+      return new Message(true, "重置密码成功");
+    } else {
+      return new Message(false, "重置密码失败");
     }
-
-    @GetMapping("/psw")
-    public String psw() {
-        return "psw";
-    }
-
-    @PostMapping("/changePsw")
-    @ResponseBody
-    public Message changePsw(String userPsw) {
-        final User me = (User) SecurityUtils.getSubject().getPrincipal();
-        User user = new User();
-        user.setUserId(me.getUserId());
-        user.setUserPsw(userPsw);
-        if (userService.changePsw(user)) {
-            return new Message(true, "重置密码成功");
-        } else {
-            return new Message(false, "重置密码失败");
-        }
-    }
-
-    @GetMapping("/me")
-    @ResponseBody
-    public User me() {
-        final User me = (User) SecurityUtils.getSubject().getPrincipal();
-        me.setUserPsw("");
-        return me;
-    }
+  }
 }
